f22b65f3ff52ca6653237899902c87dcdaf295fb
GiraFFe26 <105637726+GiraFFe26@users.noreply.github.com>
2023-03-07 18:28:02 +0700

    Add files via upload

--- 2gis.py
+++ 2gis.py
@@ -7,24 +7,21 @@ import pandas as pd
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 from selenium.webdriver.common.keys import Keys
+from datetime import date
 
 
 def collect_data(url):
     ua = UserAgent()
-    feeds, names, places, rates, towns_excel = [], [], [], [], []
+    feeds, names, places, rates, towns_excel, dates = [], [], [], [], [], []
     chromeOptions = webdriver.ChromeOptions()
     chromeOptions.add_argument("--no-sandbox")
     chromeOptions.add_argument(f'user-agent={ua.random}')
-    #chromeOptions.add_argument("--headless")
+    chromeOptions.add_argument("--headless")
     chromeOptions.add_argument('window-size=1920x1080')
     chromeOptions.add_argument('--disable-dev-shm-usage')
-    chromeOptions.add_argument('--ignore-certificate-errors')
-    chromeOptions.add_argument("--disable-blink-features=AutomationControlled")
-    chromeOptions.add_argument('--log-level=1')
-    chromeOptions.add_argument("--disable-setuid-sandbox")
-    chromeOptions.add_argument("--disable-extensions")
     chromeOptions.add_argument("--disable-gpu")
-    chromeOptions.add_argument("disable-infobars")
+    chromeOptions.add_argument("--remote-debugging-port=9231")
+    chromeOptions.binary_location = "/usr/bin/chromium-browser"
     driver = webdriver.Chrome(options=chromeOptions)
     time.sleep(3.5)
     driver.get(url)
@@ -32,10 +29,19 @@ def collect_data(url):
         driver.find_element(By.ID, 'acceptRiskButton').click()
     except NoSuchElementException:
         pass
-    with open('towns.txt', 'r', encoding='UTF-8') as file:
+    with open('/home/user/snap/maps/towns.txt', 'r', encoding='UTF-8') as file:
         towns = [i.strip() for i in file.readlines()]
     your_town = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div[2]/div/div/div[2]/div/div/div/div/div/div/div[1]/div[1]/div[3]/div[1]/h1').text.strip()
-    for town in towns[:3]:
+    for town in towns:
+        if towns.index(town) % 100 == 0:
+            driver.close()
+            driver = webdriver.Chrome(options=chromeOptions)
+            time.sleep(3.5)
+            driver.get(url)
+            try:
+                driver.find_element(By.ID, 'acceptRiskButton').click()
+            except NoSuchElementException:
+                pass
         text = f'{town} ремонт телефонов'
         driver.find_element(By.XPATH,
                             '/html/body/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div[2]/form/div/input').send_keys(text)
@@ -79,7 +85,6 @@ def collect_data(url):
             try:
                 address = to_next.find('div', class_='_4l12l8').text
             except AttributeError:
-                print(town, 'skipped')
                 for item in to_next.next_siblings:
                     if item.get('class') == None:
                         rating = item.div.div
@@ -103,14 +108,12 @@ def collect_data(url):
                             continue
                         if your_town in address:
                             break
-                        if k == 1:
-                            towns_excel.append(town)
-                        else:
-                            towns_excel.append('')
+                        towns_excel.append(town)
                         names.append(name.strip())
                         places.append(address.strip())
-                        rates.append(f'{rate} ★')
+                        rates.append(f'{rate}')
                         feeds.append(int(feed))
+                        dates.append(str(date.today()))
                         k += 1
                         if k == 12:
                             break
@@ -118,8 +121,9 @@ def collect_data(url):
             towns_excel.append(town)
             names.append(name.strip())
             places.append(address.strip())
-            rates.append(f'{rate} ★')
+            rates.append(f'{rate}')
             feeds.append(int(feed))
+            dates.append(str(date.today()))
         for item in to_next.next_siblings:
             if item.get('class') == None:
                 rating = item.div.div
@@ -143,22 +147,25 @@ def collect_data(url):
                     continue
                 if your_town in address:
                     break
-                towns_excel.append('')
+                towns_excel.append(town)
                 names.append(name.strip())
                 places.append(address.strip())
-                rates.append(f'{rate} ★')
+                rates.append(f'{rate}')
                 feeds.append(int(feed))
+                dates.append(str(date.today()))
                 k += 1
                 if k == 12:
                     break
         driver.find_element(By.XPATH,
                             '/html/body/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div[2]/form/div/div/button').click()
+    driver.close()
     df = pd.DataFrame({'Город': towns_excel,
                        'Название': names,
                        'Адрес': places,
                        'Оценка': rates,
-                       'Кол-во отзывов': feeds})
-    df.to_excel('2gis.xlsx', index=False)
+                       'Кол-во отзывов': feeds,
+                       'Дата': dates})
+    df.to_excel('/home/user/snap/maps/2gis.xlsx', index=False)
 
 
 def main():
